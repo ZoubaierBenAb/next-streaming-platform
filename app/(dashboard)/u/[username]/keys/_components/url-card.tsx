3552702728496f7f6b7996c677f6bf6ab75a18bd
weirdscenario <zoubairbenab9779@gmail.com>
2024-03-15 01:08:23 +0100

    created and completed KeyCard and UrlCard components in KeysPage

--- app/(dashboard)/u/[username]/keys/_components/url-card.tsx
+++ app/(dashboard)/u/[username]/keys/_components/url-card.tsx
@@ -9,13 +9,24 @@ interface UrlCardProps {
 
 export const UrlCard = ({ value }: UrlCardProps) => {
   return (
-    <div className="bg-muted p-6 rounded-xl my-4">
-      <div className="flex items-center gap-x-10">
-        <p className="font-semibold shrink-0">Server Url</p>
-        <Input size={2} value={value ? value : ""} disabled placeholder="Server Url" />
-        <CopyButton value = {value ? value : ''}/>
+    <div className="rounded-xl bg-muted p-6">
+    <div className="flex items-center gap-x-10">
+      <p className="font-semibold shrink-0">
+        Server URL
+      </p>
+      <div className="space-y-2 w-full">
+        <div className="w-full flex items-center gap-x-2">
+          <Input
+            value={value || ""}
+            disabled
+            placeholder="Server URL"
+          />
+          <CopyButton
+            value={value || ""}
+          />
+        </div>
       </div>
-    
     </div>
+  </div>
   );
 };
--- app/(dashboard)/u/[username]/keys/page.tsx
+++ app/(dashboard)/u/[username]/keys/page.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { getSelf } from "@/lib/auth-service";
 import { getStramByUserId } from "@/lib/stream-service";
 import { UrlCard } from "./_components/url-card";
+import { KeyCard } from "./_components/key-card";
 
 const KeysPage = async () => {
   const self = await getSelf();
@@ -14,13 +15,16 @@ const KeysPage = async () => {
 
   return (
     <div className="p-6">
-      <div className="flex-col items-center w-full justify-between mb-4">
+      <div className="flex-col items-center w-full justify-between">
         <div className="flex justify-between w-full">
           <h1 className="font-bold text-2xl">Keys & URLs</h1>
           <Button variant={"primary"}>Generate</Button>
         </div>
-      
-        <UrlCard value={stream.serverUrl} />
+      <div className="space-y-4 mt-5">
+      <UrlCard value={stream.serverUrl} />
+        <KeyCard value ={stream.streamKey}/>
+      </div>
+
         
       </div>
     </div>
